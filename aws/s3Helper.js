059700b44a172bb80dd65fb88c1a7cebacca4be0
Amit Kumar Chaudhary <113765935+chaudharytechy@users.noreply.github.com>
2024-11-23 09:57:00 +0000

    removed keys

--- aws/s3Helper.js
+++ aws/s3Helper.js
@@ -1,11 +1,6 @@
 const fs=require('fs');
 const AWS=require('aws-sdk');
 require("dotenv").config();
-AWS.config.update({
-    secretAccessKey: process.env.secretAccessKey||'5m4gjdQ3qccnByCAar/0r53E46144I1gE2o76JJF',
-    accessKeyId: process.env.accessKeyId|| 'AKIATNVEVZYXWHKME665',
-  region:process.env.region||'ap-south-1',
-})
 
 const s3=new AWS.S3();
 
